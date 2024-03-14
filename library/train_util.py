f8018f667eefe38822abf9eae418dc32258053d0
Kononenko Pavel <storuky@gmail.com>
2024-03-14 14:32:39 +0300

    Update train_util.py

--- library/train_util.py
+++ library/train_util.py
@@ -3949,7 +3949,37 @@ def get_optimizer(args, trainable_params):
 
 # Modified version of get_scheduler() function from diffusers.optimizer.get_scheduler
 # Add some checking and features to the original function.
+import math
+from torch.optim.lr_scheduler import CosineAnnealingWarmRestarts
+from torch.optim.lr_scheduler import ChainedScheduler, LinearLR
 
+
+class CosineAnnealingWarmRestartsDecay(CosineAnnealingWarmRestarts):
+  def __init__(self, optimizer, T_0, T_mult=1, eta_min=0, warmup_steps=0, last_epoch=-1, verbose=False, decay=1):
+    super().__init__(optimizer, T_0, T_mult=T_mult, eta_min=eta_min, last_epoch=last_epoch, verbose=verbose)
+    self.decay = decay
+    self.initial_lrs = self.base_lrs
+
+  def step(self, epoch=None):
+    if epoch == None:
+      if self.T_cur + 1 == self.T_i:
+        if self.verbose:
+          print("multiplying base_lrs by {:.4f}".format(self.decay))
+        self.base_lrs = [base_lr * self.decay for base_lr in self.base_lrs]
+    else:
+      if epoch < 0:
+        raise ValueError("Expected non-negative epoch, but got {}".format(epoch))
+      if epoch >= self.T_0:
+        if self.T_mult == 1:
+          n = int(epoch / self.T_0)
+        else:
+          n = int(math.log((epoch / self.T_0 * (self.T_mult - 1) + 1), self.T_mult))
+      else:
+        n = 0
+
+      self.base_lrs = [initial_lrs * (self.decay**n) for initial_lrs in self.initial_lrs]
+
+    super().step(epoch)
 
 def get_scheduler_fix(args, optimizer: Optimizer, num_processes: int):
     """
@@ -3983,8 +4013,9 @@ def get_scheduler_fix(args, optimizer: Optimizer, num_processes: int):
             values = lr_scheduler_type.split(".")
             lr_scheduler_module = importlib.import_module(".".join(values[:-1]))
             lr_scheduler_type = values[-1]
-        lr_scheduler_class = getattr(lr_scheduler_module, lr_scheduler_type)
+        lr_scheduler_class = CosineAnnealingWarmRestartsDecay# getattr(lr_scheduler_module, lr_scheduler_type)
         lr_scheduler = lr_scheduler_class(optimizer, **lr_scheduler_kwargs)
+        warmup_scheduler = WarmUpScheduler(optimizer, lr_scheduler=lr_scheduler, warmup_steps=lr_scheduler_kwargs['warmup_steps'], warmup_start_lr=1e-08, warmup_mode='linear')
         return wrap_check_needless_num_warmup_steps(lr_scheduler)
 
     if name.startswith("adafactor"):
